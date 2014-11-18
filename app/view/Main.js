8547063eb999c674e62b0f2a949cdc72faadf627
iWebi <suman.jakkula@gmail.com>
2014-11-18 09:21:04 -0600

    added styling for youtube login link

--- app/view/Main.js
+++ app/view/Main.js
@@ -10,6 +10,7 @@ Ext.define('YoutubeVideosApp.view.Main', {
     ],
     config: {
         tabBarPosition: 'bottom',
+        activeItem : 1,
         items: [
             {
                 title: 'Settings',
@@ -48,10 +49,12 @@ Ext.define('YoutubeVideosApp.view.Main', {
                         } else {
                             this.add(
                                 {
+                                    styleHtmlContent: true,
                                     html : '<a href="https://accounts.google.com/o/oauth2/auth?'+
-                                    'client_id=824411757781-kuksgb48r8qtdlrnecka855knjp68i6q.apps.googleusercontent.com&' +
-                                    'redirect_uri=http%3A%2F%2Flocalhost%3A1841&' +
-                                    'scope=https://www.googleapis.com/auth/youtube&approval_prompt=auto&response_type=token">Login To Youtube</a>'
+                                        'client_id=824411757781-kuksgb48r8qtdlrnecka855knjp68i6q.apps.googleusercontent.com&' +
+                                        'redirect_uri=http%3A%2F%2Flocalhost%3A1841&' +
+                                        'scope=https://www.googleapis.com/auth/youtube&approval_prompt=auto&response_type=token">' +
+                                    '<div class="x-button x-iconalign-left x-button-action x-layout-box-item x-flexed x-sized" style="margin: 10px !important; -webkit-box-flex: 0.1;"><span class="x-badge" style="display: none;"></span><span class="x-button-icon x-shown action"></span><span class="x-button-label" style="font-size: 1.2em">Login to youtube</span></div></a>'
                                 }
                             );
                         }
